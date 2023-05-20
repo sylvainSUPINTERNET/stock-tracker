dfcb9c3cd523f0c6c3d632ac84c340170d305634
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-05-20 16:00:03 +0200

    go

--- App.tsx
+++ App.tsx
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, extendTheme, Center, Box, HStack, VStack, Text, View} from "native-base";
+
+const newColorTheme = {
+  brand: {
+    900: "#8287af",
+    800: "#7c83db",
+    700: "#b3bef6",
+  }
+};
+const customTheme = extendTheme({ colors: newColorTheme });
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>It's me mario</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={customTheme}>
+
+      {/* <View style={styles.container}> */}
+
+      <View style={{marginTop: 35}}>
+
+        <HStack>
+          <Box flex={1} bg={"amber.100"} style={{"padding": 20}}>
+            <Center>
+              <Text fontSize="4xl" >STOCK TRACKER</Text>
+            </Center>
+          </Box>
+        </HStack>
+
+      </View>
+
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ firebase/firebase.ts
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+
+
+const firebaseConfig = {
+
+};
+
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
