ba8c5c507cb58d1fb3a3b045168fe8a3617bc78b
sylvain joly <sylvain.joly00@orange.fr>
2023-05-21 03:53:08 +0200

    [KEP

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import firebase from "firebase/compat";
-import { NativeBaseProvider, extendTheme, Center, Box, HStack, Text, View, FormControl, Stack, Input, Container, Button, Icon} from "native-base";
-import React, { useEffect } from "react";
+import { NativeBaseProvider, extendTheme, Center, Box, HStack, Text, View, FormControl, Stack, Input, Container, Button, Icon, Modal, WarningOutlineIcon, Spinner, useToast, Card, ScrollView} from "native-base";
+import React, { useEffect, useReducer, useState } from "react";
 import { firebaseConfig } from "./firebase/firebase";
 import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
+
 const newColorTheme = {
   brand: {
     900: "#8287af",
@@ -13,52 +14,156 @@ const newColorTheme = {
 };
 const customTheme = extendTheme({ colors: newColorTheme });
 
-const addStock = (ev:any) => {
-  console.log("addstock");
-}
 
 export default function App() {
 
+  const toast = useToast();
+
+  const [showModal, setShowModal] = useState(false);
+  const [isInvalidForm, setIsInvalidForm] = useState(false);
+  const [justSubmited, setJustSubmited] = useState<undefined|boolean>(undefined);
+  
+
+  // modal form
+  const [stockName, setStockName] = useState('');
+  const [toInvest, setToInvest] = useState('');
+
+
+  // investments
+  const [listInvestments, setListInvestments] = useState<any[]>([]);
+
+
+  const addStock = (ev:any) => {
+    setShowModal(true);
+  }
+
+  const handleSubmit = async (ev:any) => {
+    
+    if ( toInvest === "" || stockName === "" ) {
+      setIsInvalidForm(true);
+      return;
+    }
+
+    if ( isInvalidForm ) {
+      return;
+    }
+
+    setJustSubmited(true);
+
+
+    try {
+      const symbol = stockName.toUpperCase();
+
+      await firebase.firestore()
+      .collection("investments")
+      .doc(stockName)
+      .set({
+        stockName: symbol,
+        investAmount: toInvest,
+        urlTrackDetail: `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?modules=price`
+      });
+
+      setTimeout( async () => {
+
+        setStockName("");
+        setToInvest("");
+
+        setShowModal(false);
+        setJustSubmited(undefined);
+        const querySnapshot = await firebase.firestore().collection("investments").get();
+        setListInvestments([...querySnapshot.docs.map( (doc:any) => doc.data())])
+      }, 1500);
+
+    } catch ( error ) {
+      console.log(error);
+      toast.show({
+        title: "Error",
+        description: "Error while adding stock : " + error
+      });
+
+    }
+
+
+  }
+
+  const getFormValue = (ev:any, field:"toInvest" | "stockName") => {
+
+    setJustSubmited(false);
+
+    if ( field === "toInvest" ) {
+      if ( isNaN(ev.nativeEvent.text) ) {
+        setIsInvalidForm(true);
+      } else {
+        setIsInvalidForm(false);
+      }
+      setToInvest(ev.nativeEvent.text);
+    }
+
+    if ( field === "stockName" ) {
+      setStockName(ev.nativeEvent.text);
+    }
+  }
+  
+  
   useEffect (() => {
-
-
     firebase.initializeApp(firebaseConfig);
 
-    // firebase.firestore().collection("users").add({
-    //   "name": "toto"
-    // })
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await firebase.firestore().collection("investments").get();
+        setListInvestments([...querySnapshot.docs.map( (doc:any) => doc.data())])
+      } catch ( e ) {
+        toast.show({
+          title: "Error",
+          description: "Error while fetching data : " + e
+        });
+      }
+    }
 
-    firebase
-    .firestore()
-    .collection("XDe")
-    .doc("TOTO").collection("stocks").add({ name: "toto" });
-
-    // const usersRef = firebase.database().ref('users');
-
-    // // Generate a unique key for the new user
-    // const newUserKey = usersRef.push().key;
-    
-    // // Create the user object with the name "toto"
-    // const user = {
-    //   name: 'toto'
-    // };
-    
-    // // Set the user object at the generated key
-    // usersRef.child(newUserKey!).set(user)
-    //   .then(() => {
-    //     console.log('User inserted successfully!');
-    //   })
-    //   .catch((error:any) => {
-    //     console.error('Error inserting user:', error);
-    //   });
-  });
+    fetchData();
+  }, []);
 
   return (
     <NativeBaseProvider theme={customTheme}>
 
       {/* <View style={styles.container}> */}
 
-      <View style={{marginTop: 35}}>
+      <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+        <Modal.CloseButton style={{marginTop:120}}/>
+        <Modal.Content maxWidth="900px">
+          <Modal.Header >Stock</Modal.Header>
+          <Modal.Body>
+            <FormControl isInvalid={isInvalidForm}>
+              <Box>
+                <Input variant="outline" placeholder="Stock name ( Symbol ) " style={{fontSize:16}} value={stockName}
+                onChange={(e:any) => getFormValue(e, "stockName")}/>
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                  Must be valid name
+                </FormControl.ErrorMessage>
+              </Box>
+              <Box style={{marginTop:15}}>
+                <Input variant="outline" placeholder="To invest" style={{fontSize:16}} value={toInvest}
+                onChange={(e:any) => getFormValue(e, "toInvest")}/>
+                <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+                  Must be valid price
+                </FormControl.ErrorMessage>
+              </Box>
+            </FormControl>
+          </Modal.Body>
+          <Modal.Footer>
+            <Box bg={"purple.300"} flex={1}>
+              <Button onPress={handleSubmit} isDisabled={isInvalidForm || justSubmited}>
+                {
+                  justSubmited ? <Spinner></Spinner> : <Text color={"white"}>Confirm</Text>
+                }
+              </Button>
+            </Box>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+
+      <View style={{marginTop: 30}}>
+
 
       <LinearGradient colors={['#8EC5FC', '#E0C3FC', '#FF8EF7']}>
         <Box style={{ padding: 20 }}>
@@ -70,28 +175,34 @@ export default function App() {
 
 
       <Box style={{display:"flex", justifyContent: "center", marginTop: 20}}>
-        <Button style={{"marginRight": 80, marginLeft: 80}} variant="outline" onPress={addStock} colorScheme="secondary">
+        <Button style={{"marginRight": 80, marginLeft: 80}} variant="outline" onPress={addStock} colorScheme="primary">
           <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Icon as={Ionicons} name="add" color={"purple.900"} size={6}/>
-            <Text style={{ fontSize: 16, color: 'purple', fontWeight: 'bold', marginLeft: 3 }}>
+            <Icon as={Ionicons} name="add" color={"blue.900"} size={6}/>
+            <Text style={{ fontSize: 16, color: '#3461eb', fontWeight: 'bold', marginLeft: 3 }}>
               STOCK
             </Text>
           </View>
         </Button>
       </Box>
-
-
-
-
-      {/* <Box>
-        <Stack style={{marginRight:50, marginLeft:50, marginTop:40, marginBottom:40}}>
-            <FormControl>
-              <Input variant="outline" placeholder="Action name" style={{fontSize:26}}/>
-            </FormControl>
-        </Stack>
-      </Box> */}
-
       </View>
+
+
+    {
+      listInvestments.length === 0 ? <Box marginTop={10}>
+        <Text textAlign="center" fontSize={"2xl"} style={{fontWeight: "bold"}}>No stock trackers for the moment ...</Text></Box> : 
+        <ScrollView style={{marginTop: 25}}>
+          {
+            listInvestments.map((investElement) => (
+                <Box key={investElement.id} marginLeft={2} marginRight={2} marginBottom={2} bg={"purple.200"} padding={4} rounded={"2xl"}>
+                  <Text style={{fontSize: 20, fontWeight: "bold"}}>{investElement.stockName}</Text>
+                  <Text style={{fontSize: 16, fontWeight: "bold"}}>Invested : {(investElement.investAmount).toLocaleString("fr-FR", { style: "currency", currency: "EUR" })} EUR</Text>
+                </Box>
+            ))
+          }
+        </ScrollView>
+    }
+
+
 
     </NativeBaseProvider>
   );
